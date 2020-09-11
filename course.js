bf280bd29fa4bae45aa4203fe84960783d8d33fc
Ivan Sam <sitex@users.noreply.github.com>
2020-09-11 18:27:59 -0500

    add Docs

--- course.js
+++ course.js
@@ -1,3 +1,7 @@
+/**
+ * Source from JSLists
+ * @link https://www.cssscript.com/demo/collapsible-folder-tree-pure-javascript-jslists/
+ */
 (function() {
     "use strict";
 
@@ -20,8 +24,6 @@
                 t = ".jslist-li{margin-left: 22px;}";
 			t += "ul, li { list-style: none; margin: 5px 0; padding: 0; }";
 			t += "ul.jslist-ul, li.jslist-li { margin-left: 22px; }";
-			t += ".jslist-li{margin-left: 22px;}";
-			t += ".jslist-li{margin-left: 22px;}";
             t += ".jslist-ul{margin-left: 22px;}", t += ".jslist-ol{margin-left: 22px;}", t += ".jsl-collapsed{display: none;}", t += ".jsl-collapsed-arrow{float: left; clear: both; margin-right: 11px; width: 11px; height: 11px; cursor: pointer; background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAALCAYAAACprHcmAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAKJJREFUeNqEkc0JxCAQhUfRg7EJweN2IikoJWxBkm4EexCvur4gi4kuO/Dw532O48i89y8ieje5Jk5zlKaz6RAArbW7MYYYYxNZa+Uxxj2EcGVyALsxCdF9B5gj4wgopW7rfiNf1fgzxFAbaa2/xrZt15hznuHRwKERWmZ+dGEJF7RnNFNKN7jPCx54tj6SEIKklJOwDx8cMh9oeNPfH/wIMABbu2PPHYUsbQAAAABJRU5ErkJggg==') no-repeat;}", t += ".jsl-open{display: block;}", t += ".jsl-open-arrow{background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAALBAMAAABbgmoVAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAACRQTFRFtLS0tLS0tLS0AAAAtLS019fX8/Pz9PT09fX19vb29/f3+Pj4NWr6kwAAAAN0Uk5TK7P9wooeBQAAAD9JREFUCNdjYFRxcVFkYBBdvXv3UgEGrVVAoMDgPXPmzNkOcKrZ2LgbSHV0dICo8vLyagcGrbS0tCwFmD6IKQDtKxlF/vrVqgAAAABJRU5ErkJggg==') no-repeat;}", e.styleSheet ? e.styleSheet.cssText = t : e.appendChild(document.createTextNode(t)), document.getElementsByTagName("head")[0].appendChild(e)
         }, e.paddULLists = function(e) {
             var t, l = document.getElementById(e).getElementsByTagName("UL");
@@ -75,16 +77,22 @@
     "undefined" == typeof JSLists ? window.JSLists = e() : console.log("JSLists already defined.")
 })();
 
-var makeRequest = function(url, callback) {
+// The end of JSLists
+// ----------------------------------------------------
+
+/**
+ * Get JSON from URL 
+ * @param {string} url 
+ * @param {Function} callback 
+ */
+function getJSON(url, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url, true);
 	xhr.responseType = 'json';
 
 	xhr.onload = function() {
-
 		var status = xhr.status;
-		
-		if (status == 200) {
+		if (status === 200) {
 			callback(null, xhr.response);
 		} else {
 			callback(status);
@@ -94,23 +102,22 @@ var makeRequest = function(url, callback) {
 	xhr.send();
 };
 
-var sources;
-var playlist;
-var data_json = 'https://gist.githubusercontent.com/sitex/d52e7053dd32c4bbc599d88282eb5410/raw/playlist.json';
-makeRequest(data_json,  function(err, data) {
-	if (err != null) {
-		console.error(err);
-	} else {
-		sources = data.my_tree.sources;
-		playlist = data;
-		// console.log(playlist);
-		renderHello();
-	}
-});
 
-function renderHello() {
+/**
+ * Render Navigation using data from json
+ * 
+ * @param {object} playlist 
+ * @param {HTMLElement} target 
+ */
+function renderNavigation(playlist, target) {
 	var player = '';
 
+    /**
+     * Prepare HTML for the element by its properties
+     * 
+     * @param {object} child
+     * @returns {string}
+     */
 	function renderChild(child) {
 		var out = '';
 
@@ -129,7 +136,6 @@ function renderHello() {
 			out += '</a></span>';
 
 		} else if (child.hasOwnProperty('file')) {
-			player = (child.file.hasOwnProperty('player')) ? child.file.player : '';
 			out += '<span class="link">';
 			out += '<a class="tocLink" href="javascript:void(0);"';
 			out +=' onclick="javascript:videoGoTo' + player;
@@ -144,12 +150,19 @@ function renderHello() {
 		return out;
 	}
 
+    /**
+     * Prepare HTML for all elements
+     * 
+     * @param {array} children 
+     * @param {int} level
+     * @returns {string}
+     */
 	function renderChildren(children, level) {
 		var out = (level === 0) ? '<ul id="simple_list">' : '<ul>';
 		level++;
 		children.forEach( function(child) {
-			if (level === 4 && child.hasOwnProperty('file')) {
-				child.file.player = 2;
+			if (child.hasOwnProperty('file')) {
+				child.file.player = (level === 4) ? '2' : '';
 			}
 			out += "<li>";
 			out += renderChild(child);
@@ -161,13 +174,30 @@ function renderHello() {
 		return out + "</ul>";
 	};
 
-	var level = 0;
-    var rendered = renderChildren(playlist.my_tree.collection, level);
-    var target = document.getElementById("tocContainer");
-
     if (target) {
-      target.innerHTML = rendered;
+        var level = 0; // default
+        target.innerHTML = renderChildren(playlist.my_tree.collection, level);;	
     }
-	
-	JSLists.applyToList('simple_list', 'ALL');
 }
+
+var data_json = 'https://gist.githubusercontent.com/sitex/d52e7053dd32c4bbc599d88282eb5410/raw/playlist.json';
+getJSON(data_json, function(err, data) {
+	if (err != null) {
+		console.error(err);
+	} else {
+        // Overwrite sources variable
+		window.sources = data.my_tree.sources;
+        // show Navigation
+        renderNavigation(data, document.getElementById("tocContainer"));
+        // use JSList plugin for Navigation
+        JSLists.applyToList('simple_list', 'ALL');
+	}
+});
+
+/**
+ * Add styles for Navigation
+ */
+var style_tag = document.createElement("style");
+var nav_styles = "ul, li { list-style: none; margin: 5px 0; padding: 0; }";
+style_tag.appendChild(document.createTextNode(nav_styles))
+document.getElementsByTagName("head")[0].appendChild(style_tag);
